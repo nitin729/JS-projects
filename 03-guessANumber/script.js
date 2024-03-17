1d6c2df338e2471acba6ee7ed25fd11015d9576f
nitin729 <redruby@tuta.io>
2024-03-17 13:34:58 +0530

    refactored the number guessing game

--- 03-guessANumber/script.js
+++ 03-guessANumber/script.js
@@ -1,43 +1,93 @@
-//TODO: Refactor this code
-
-const prevGuesses = [];
-const randomNumber = Math.floor(Math.random() * 100 + 1);
-const guesses = document.querySelector(".guesses");
 const form = document.querySelector(".form");
+const input = document.querySelector("#guessField");
+const submit = document.querySelector("#subt");
+const guesses = document.querySelector(".guesses");
+const lowOrHi = document.querySelector(".lowOrHi");
+const lastRes = document.querySelector(".lastResult");
+const startOver = document.querySelector(".resultParas");
+const p = document.createElement("p");
+let prevGuesses = [];
 let attempts = 10;
-form.addEventListener("submit", function guessNumber(event) {
-  event.preventDefault();
+let playGame = true;
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log(playGame);
+if (playGame) {
+  submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    const guess = parseInt(input.value);
+    validateGuess(guess);
+    input.value = "";
 
-  const input = parseInt(document.querySelector("#guessField").value);
-  const winner = document.querySelector(".winner");
-  const lowOrHi = document.querySelector(".lowOrHi");
-  const lastRes = document.querySelector(".lastResult");
-  if (attempts > 0) {
-    console.log(input);
-    lowOrHi.innerText = "";
-    if (input === "" || input < 0 || input > 100 || isNaN(input)) {
-      alert("Invalid Input");
-      return;
-    }
-    if (randomNumber < input) {
-      lowOrHi.innerText = `Your number is greater than the actual number`;
-    }
-    if (randomNumber > input) {
-      lowOrHi.innerText = `Your number is lesser than the actual number`;
-    }
-    if (randomNumber === input) {
-      winner.innerText = `You won. The number was ${randomNumber}`;
-      return;
-    }
-    prevGuesses.push(input);
-    attempts--;
-    input = null;
-    lastRes.innerText = attempts;
-    guesses.innerText = prevGuesses;
+    console.log(randomNumber, guess);
+  });
+}
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("Please enter a valid number");
+  } else if (guess < 0) {
+    alert("Please enter a number greater than 0");
+  } else if (guess > 100) {
+    alert("Please enter a number less than 100");
   } else {
-    winner.innerText = `You have exceeded your chances`;
-    return;
+    if (attempts >= 1) {
+      prevGuesses.push(guess);
+      compareGuesses(guess);
+      displayGuess();
+    } else {
+      lowOrHi.innerText = `You Lost, random number was ${randomNumber}`;
+      displayGuess();
+      endGame();
+    }
   }
+}
 
-  console.log(randomNumber);
-});
+function compareGuesses(guess) {
+  let msg;
+  if (guess > randomNumber) {
+    msg = `Your number is greater than the actual number`;
+    displayMessage(msg);
+  } else if (guess < randomNumber) {
+    msg = `Your number is lesser than the actual number`;
+    displayMessage(msg);
+  } else {
+    msg = `You Won, the random number was ${randomNumber}`;
+    lowOrHi.innerText = msg;
+    endGame();
+  }
+  attempts--;
+  lastRes.innerText = attempts;
+}
+
+function displayGuess(guess) {
+  guesses.innerText = prevGuesses;
+}
+
+function displayMessage(msg) {
+  lowOrHi.innerText = msg;
+}
+
+function endGame() {
+  input.value = "";
+
+  input.setAttribute("disabled", true);
+  p.classList.add("button");
+  p.innerHTML = `<button id='newGame'> New Game </button>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+  const btn = document.querySelector("#newGame");
+  btn.addEventListener("click", function (event) {
+    attempts = 10;
+    prevGuesses = [];
+    randomNumber = Math.floor(Math.random() * 100 + 1);
+    lowOrHi.innerText = "";
+    input.removeAttribute("disabled");
+    guesses.innerText = "";
+    startOver.removeChild(p);
+    playGame = true;
+  });
+}
